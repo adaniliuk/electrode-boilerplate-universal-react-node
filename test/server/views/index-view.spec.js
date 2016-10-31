b65f7904f12ff0abbf01584a61364b710e34a8cb
Andrei Daniliuk <adaniliuk@gmail.com>
2016-10-30 20:12:35 -0700

    Added server side unit test case

---
+++ test/server/views/index-view.spec.js
@@ -0,0 +1,7 @@
+const IndexView = require("../../../server/views/index-view.jsx");
+
+describe("home-view.jsx", () => {
+  it("IndexView is loaded", () => {
+    expect(IndexView).to.be.defined;
+  });
+});
